a45423aa8dadda89d40c5854c18b09ff925d2229
devmagda <113769198+devmagda@users.noreply.github.com>
2023-09-18 20:21:59 +0200

    Added DeepFace functions

--- face-detection/Human.py
+++ face-detection/Human.py
@@ -3,7 +3,7 @@ import ImageUtils
 from ImageUtils import ImageDetectionUtil
 from Shapes import Rectangle
 
-class Faces():
+class Faces:
 
     @staticmethod
     def getValidFaces(gray, eyeCascade, faceCascade, scale=1.0):
@@ -39,9 +39,114 @@ class Faces():
     @staticmethod
     def getFromImg(gray, cascade):
         return ImageDetectionUtil.getObjectByCascade(cascade, gray, color=Constants.FACES_COLOR)
+    actions = ['age', 'gender', 'race', 'emotion']
+
+    models = [
+        'VGG-Face',
+        'Facenet',
+        'Facenet512',
+        'OpenFace',
+        'DeepFace',
+        'DeepID',
+        'ArcFace',
+        'Dlib',
+        'SFace',
+    ]
+    model_name = models[0]
+
+    metrics = ['cosine', 'euclidean', 'euclidean_l2']
+    distance_metric = metrics[0]
+
+    backends = [
+        'opencv',
+        'ssd',
+        'dlib',
+        'mtcnn',
+        'retinaface',
+        'mediapipe',
+        'yolov8',
+        'yunet',
+    ]
+    detector_backend = backends[4]
+
+    norms = [
+        'base',
+        'raw',
+        'Facenet',
+        'Facenet2018',
+        'VGGFace',
+        'VGGFace2',
+        'ArcFace'
+    ]
+    normalization = norms[2]
+
+    cached_face_data = None
+
+    @staticmethod
+    def initialize(face):
+        Faces.cached_face_data = Faces.analyze(face, actions=['gender', 'race'])
+
+    @staticmethod
+    def represent(image):
+        from deepface import DeepFace
+        model = DeepFace.represent(
+            image,
+            model_name=Faces.model_name,
+            detector_backend=Faces.detector_backend,
+            normalization=Faces.normalization
+        )
+        return model
+
+    @staticmethod
+    def analyze(image, actions=['age', 'gender', 'race', 'emotion']) -> tuple[str, str, {}]:
+        from deepface import DeepFace
+        data = DeepFace.analyze(
+            image,
+            actions=actions,
+            detector_backend=Faces.detector_backend,
+            silent=True
+        )
+        return data[0]
 
 
-class Eyes():
+    @staticmethod
+    def get_race(image):
+        data = Faces.analyze(image, actions=['race'])
+        return data['dominant_race']
+
+    @staticmethod
+    def get_gender(image):
+        data = Faces.analyze(image, actions=['gender'])
+        return data['dominant_gender']
+
+    @staticmethod
+    def get_age(image):
+        data = Faces.analyze(image, actions=['age'])
+        return data['age']
+
+    @staticmethod
+    def get_emotion(image):
+        data = Faces.analyze(image, actions=['emotion'])
+        return data['dominant_emotion']
+
+    @staticmethod
+    def compare(face1, face2) -> tuple[int, float, float]:
+        from deepface import DeepFace
+        try:
+            result = DeepFace.verify(
+                img1_path=face1,
+                img2_path=face2,
+                model_name=Faces.model_name,
+                distance_metric=Faces.distance_metric,
+                detector_backend=Faces.detector_backend,
+                enforce_detection=True,
+                normalization=Faces.normalization)
+            return int(result['verified']), result['distance'], result['threshold']
+        except ValueError:
+            return -1, 0.0, 0.0
+
+
+class Eyes:
 
     @staticmethod
     def getFromImg(image, cascade, offset=[0, 0]):
